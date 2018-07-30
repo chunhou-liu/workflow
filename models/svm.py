7107af7846b429f7631ea28bf11f8d60564ba0a3
liuchunhou <liuchunhou@gmail.com>
2018-07-30 22:16:58 +0800

    svm

--- models/svm.py
+++ models/svm.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
-import sys
+import argparse
 from datetime import datetime
 import numpy as np
 from sklearn import svm, linear_model
 from sklearn.utils import resample
-from datasets.mnist import mnist
+from datasets.mnist import mnist as MNIST
+
+
+mnist = MNIST(False)
 
 
 def log(*arg, **kwarg):
-    print(*arg, **kwarg)
+    print(*arg, **kwarg, sep='\t')
     with open("svm-log.txt", "a") as f:
-        print(*arg, **kwarg, file=f)
+        print(*arg, **kwarg, file=f, sep='\t')
 
 
 def svm_classifier(X, y):
@@ -18,7 +21,8 @@ def svm_classifier(X, y):
     start = datetime.now()
     clf.fit(X, y)
     end = datetime.now()
-    return clf, (end-start).total_seconds()
+    acc = np.sum(clf.predict(mnist.test.images) == mnist.test.labels) / len(mnist.test.labels)
+    return (end-start).total_seconds(), acc
 
 
 def regression(x, y):
@@ -26,36 +30,36 @@ def regression(x, y):
     y = np.array(y)
     model = linear_model.LinearRegression()
     model.fit(x, y)
-    log("model coef:", model.coef_, "model intercept:", model.intercept_, sep='\t')
+    log(model.coef_, model.intercept_)
     return model
 
-"""
-assert(all([i.isdigit() for i in sys.argv[1:]]))
 
-sample_numbers = [int(i) for i in sys.argv[1:-3]]
-assert(all([i <= len(mnist.train.images) for i in sample_numbers]))
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mark", action="store_true")
+    parser.add_argument("--size", type=int, nargs="+", default=[1000])
+    parser.add_argument("--predict", type=int, nargs="+", default=[2000])
+    args = parser.parse_args()
+    if args.mark:
+        train_times = []
+        for sample_num in args.size:
+            x, y = resample(mnist.train.images, mnist.train.labels, replace=False, n_samples=sample_num)
+            train_time, acc = svm_classifier(x, y)
+            train_times.append(train_time)
+            log(sample_num, train_time, acc)
+        log("%"*50)
+        model = regression(args.size, train_times)
+        for sample_num in args.predict:
+            x, y = resample(mnist.train.images, mnist.train.labels, replace=False, n_samples=sample_num)
+            train_time, acc = svm_classifier(x, y)
+            train_times.append(train_time)
+            log(sample_num, model.predict(sample_num**2), train_time, acc)
+    else:
+        for sample_num in args.predict:
+            x, y = resample(mnist.train.images, mnist.train.labels, replace=False, n_samples=sample_num)
+            train_time, acc = svm_classifier(x, y)
+            train_times.append(train_time)
+            log(sample_num, train_time, acc)
+    log("#"*50)
 
-train_times = []
-for sample_num in sample_numbers:
-    x, y = resample(mnist.train.images, mnist.train.labels, replace=False, n_samples=sample_num)
-    model, train_time = svm_classifier(x, y)
-    acc = np.sum(model.predict(mnist.test.images) == mnist.test.labels) / len(mnist.test.labels)
-    log(sample_num, train_time, acc, sep='\t')
-    train_times.append(train_time)
-
-log(sample_numbers)
-log(train_times)
-log("+"*50)
-
-model = regression(sample_numbers, train_times)
-
-test_sample_numbers = [int(i) for i in sys.argv[-3:]]
-
-for sample_num in test_sample_numbers:
-    x, y = resample(mnist.train.images, mnist.train.labels, replace=False, n_samples=sample_num)
-    model, train_time = svm_classifier(x, y)
-    acc = np.sum(model.predict(mnist.test.images) == mnist.test.labels) / len(mnist.test.labels)
-    log("prediction time:",model.predict(sample_num**2), "real time:", train_time, "acc:", acc, sep='\t')
-
-log("="*50)
-"""
+# python -m models.svm --mark --size 1000 5000 10000 15000 20000 25000 30000 35000 40000 45000 50000 55000 --predict 2000 17000 47000 35000
